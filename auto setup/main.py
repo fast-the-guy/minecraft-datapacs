3a2a6ed4a9859d3f7bd80b1725dbe2ef07ac385f
fast-the-guy <brage.alexander.jahr+github@gmail.com>
2025-06-24 22:47:14 +0200

    vs code

---
+++ auto setup/main.py
@@ -0,0 +1,16 @@
+"""
+a python script to make datapack setup/creation easier
+"""
+
+
+
+# get info from user/hardcoded
+print("Welcome to the datapack setup script!")
+pack_name = input("Enter the name of your datapack: ")
+pack_description = input("Enter a description for your datapack: ")
+pack_author = "Fast_the_guy"    # hardcoded because I am lazy/only i use this script
+pack_namespace = input("Enter the namespace for your datapack: ")
+
+
+
+# create the datapack folder structure
